1e16e27a21c6b78ec5e901dbc05e5fdfbaf44802
Euan Barclay <Silverswimmer63@gmail.com>
2020-10-27 12:35:53 -0400

    class and home work

--- javascript.js
+++ javascript.js
@@ -8,7 +8,8 @@ document.getElementById("feedback").innerHTML = output;
 if this does not show up, and your expected result does not happen, then you
 have an issue bellow. --------------------------------------------------------*/
 
-/*output = [];
+/* 10/26/20 Work
+output = [];
 var arrOne = [1,2,3,4,5];
 output.push(arrOne.pop());
 output.push(arrOne.pop());
@@ -16,18 +17,40 @@ output.push(arrOne.pop());
 output.push(arrOne.pop());
 output.push(arrOne.pop());
 //output = arrOne.reverse();
-*/
 
-/*
 var arrTwo = ["I" + "desire" + "to" + "be" + "a" + "sentence."];
 output = arrTwo.join(" ");
-*/
 
-/*
 var strOne = "I really wish I were an array";
 output = strOne.split(" ");
 console.log(output);
 */
+
+/*10/27/20 Homework
+var startcoords = {x:5, y:5};
+var endcoords = {x:0, y:7};
+// startcoords.x and endcoords.y
+
+var arr = [1, 2, 3, 4];
+output.push(arr.pop());
+output.push(arr.pop());
+output.push(arr.pop());
+output.push(arr.pop());
+
+class Student {
+  constructor(name, age, year, major){
+    this.name = name;
+    this.age = age;
+    this.year = year;
+    this.major = major;
+  }
+}
+
+var studentInfo = ["J.R. Bob Dobbs", 17, 2021, "Computer Science"];
+// when creating a student, just add these parameters.
+*/
+
+
 
 /*----------------------------------------- Do not make changes below this line
 Output command for any changes above. If there are no changes above then it
