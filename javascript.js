ee3a25bca2356b51b3506eb0f6722a540eb72a06
Euan Barclay <Silverswimmer63@gmail.com>
2020-10-28 12:05:11 -0400

    class work from 10/28/20

--- javascript.js
+++ javascript.js
@@ -8,49 +8,7 @@ document.getElementById("feedback").innerHTML = output;
 if this does not show up, and your expected result does not happen, then you
 have an issue bellow. --------------------------------------------------------*/
 
-/* 10/26/20 Work
-output = [];
-var arrOne = [1,2,3,4,5];
-output.push(arrOne.pop());
-output.push(arrOne.pop());
-output.push(arrOne.pop());
-output.push(arrOne.pop());
-output.push(arrOne.pop());
-//output = arrOne.reverse();
-
-var arrTwo = ["I" + "desire" + "to" + "be" + "a" + "sentence."];
-output = arrTwo.join(" ");
-
-var strOne = "I really wish I were an array";
-output = strOne.split(" ");
-console.log(output);
-*/
-
-/*10/27/20 Homework
-var startcoords = {x:5, y:5};
-var endcoords = {x:0, y:7};
-// startcoords.x and endcoords.y
-
-var arr = [1, 2, 3, 4];
-output.push(arr.pop());
-output.push(arr.pop());
-output.push(arr.pop());
-output.push(arr.pop());
-
-class Student {
-  constructor(name, age, year, major){
-    this.name = name;
-    this.age = age;
-    this.year = year;
-    this.major = major;
-  }
-}
-
-var studentInfo = ["J.R. Bob Dobbs", 17, 2021, "Computer Science"];
-// when creating a student, just add these parameters.
-*/
-
-
+var euanTrivia = [{statment: "The sky is yellow", value: false}, {statment: "Memphis is tall", value: false}, {statment: "Euan is bigbrain", value: false}, {statment: "Euan drives a van", value: true}, {statment: "Magenta is the best color", value: true}, {statment: "Disney ruined Star Wars", value: true}, {statment: "Minecraft is the GOAT", value: true}, {statment: "Fortnight is trash", value: true}, {statment: "Aidan can drive", value: false}, {statment: "Zoom is a great learning platform: ", value: false}]
 
 /*----------------------------------------- Do not make changes below this line
 Output command for any changes above. If there are no changes above then it
---
+++ oldStuf.js
@@ -0,0 +1,66 @@
+//10/26/20 Work
+
+output = [];
+var arrOne = [1,2,3,4,5];
+output.push(arrOne.pop());
+output.push(arrOne.pop());
+output.push(arrOne.pop());
+output.push(arrOne.pop());
+output.push(arrOne.pop());
+//output = arrOne.reverse();
+
+var arrTwo = ["I" + "desire" + "to" + "be" + "a" + "sentence."];
+output = arrTwo.join(" ");
+
+var strOne = "I really wish I were an array";
+output = strOne.split(" ");
+console.log(output);
+
+/*----------------------------------------------------------------------------*/
+//10/27/20 Homework
+
+/* Instructions
+If we had 2 prototype objects:
+
+startCords = {x:5, y:5};
+endCords = {x: 0, y:7};
+
+How would we get the change in x? The change in y?
+
+If I had the array [1,2,3,4]
+How would I reverse it and store it in output?
+
+How would you make a class Student who is constructed with name, age, year, and major?
+
+If you had the array studentInfo = [“J.R. Bob Dobbs”, 17, 2021, “Computer Science”]
+
+And you were making a new object of the student class above, named student how
+would you use array in making that object?
+
+*/
+var startcoords = {x:5, y:5};
+var endcoords = {x:0, y:7};
+var xchag = Mat.abs(startcoords.x - endcoords.x);
+var ychag = Mat.abs(startcoords.y - endcoords.y);
+
+var arr = [1, 2, 3, 4];
+output.push(arr.pop());
+output.push(arr.pop());
+output.push(arr.pop());
+output.push(arr.pop());
+
+class Student {
+  constructor(name, age, year, major){
+    this.name = name;
+    this.age = age;
+    this.year = year;
+    this.major = major;
+  }
+}
+
+var studentInfo = [];
+studentInfo = ["J.R. Bob Dobbs", 17, 2021, "Computer Science"];
+var kid = new Student(studentInfo[0], studentInfo[1], studentInfo[2], studentInfo[3]);
+// when creating a student, just add these parameters.
+
+/*----------------------------------------------------------------------------*/
