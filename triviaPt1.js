dc65e12300f4fe8f435e4bc797b53d8c4a3e29b5
Silverswimmer63 <54989699+Silverswimmer63@users.noreply.github.com>
2020-11-02 16:57:17 -0500

    tried what Memphis did, gave empty arrays

--- triviaPt1.js
+++ triviaPt1.js
@@ -1,11 +1,17 @@
 
 function randomList(length) { //function name with designated perameter
-  var list = length;
-  for (var i = 0; i < list.length; i++) {
-    list;
+  var itter = length;
+  var list = [];
+  for (var j = 0; j < itter.length; j++) {
+    list.push(j);
   }
+  console.log(list);
+  var spliced = [];
+  var index = list.length;
   for (var i = 0; i < list.length; i++) {
-    var retArr = list.splice(Math.floor(Math.random() * list.length));
+    var randMath = Math.floor(Math.random() * (list.length - 1));
+    var newList = list.splice(randMath, 1);
+    spliced.push(newList[0]);
   }
-  console.log(retArr);
+  return spliced;
 }
