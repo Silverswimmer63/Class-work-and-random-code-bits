fd9ec9d24f50f9c0f953c83f26c5aad08fc95724
Silverswimmer63 <54989699+Silverswimmer63@users.noreply.github.com>
2020-11-09 16:20:57 -0500

    crazy number of things fix

--- triviaPt1.js
+++ triviaPt1.js
@@ -1,6 +1,6 @@
 /*
- Memphis:
-Aidan doesn’t take physics but acts like he does: T
+ Memphis: 
+Aidan doesn’t take physics but acts like he does: T  
 Global warming is a lie: F
 Bethesda made the biggest flop in gaming history:T
 PC isn’t master race: F
@@ -20,22 +20,22 @@ console.log(display.textContent)
 var dochandeler = document.getElementById("textbox");
 
 
-var myStatments = [{statement: "Aidan doesn't take physics but acts like he does", value: true},
-                   {statement: "Global warming is a lie", value: false},
-                   {statement: "Bethesda made the biggest flop in gaming history", value: true},
-                   {statement: "PC isn't master race", value: false},
-                   {statement: "Aidan calls any sport with a ball in it sports ball", value: true},
-                   {statement: "Aidan is Extreme big brain", value: false},
-                   {statement: "Komoto best code editor", value: true},
-                   {statement: "Windows 10 is good", value: false},
-                   {statement: "Euan hates on Komoto because why not", value: true},
-                   {statement: "Aidan thinks PC isn't master race", value: false}];
+var myStatments = [{statement: "Aidan doesn't take physics but acts like he does", bool: true},
+                   {statement: "Global warming is a lie", bool: false},
+                   {statement: "Bethesda made the biggest flop in gaming history", bool: true},
+                   {statement: "PC isn't master race", bool: false},
+                   {statement: "Aidan calls any sport with a ball in it sports ball", bool: true},
+                   {statement: "Aidan is Extreme big brain", bool: false},
+                   {statement: "Komoto best code editor", bool: true},
+                   {statement: "Windows 10 is good", bool: false},
+                   {statement: "Euan hates on Komoto because why not", bool: true},
+                   {statement: "Aidan thinks PC isn't master race", bool: false}];
 
 function randomList(lists, length) { //function name with designated perameter
   var itter = length;
   var list = [];
   for (var j = 0; j < itter; j ++ ) {
-           list.push(j);
+           list.push(j);        
   }
   var splicedstuff = [];
   var index = list.length;
@@ -51,7 +51,7 @@ function randomList(lists, length) { //function name with designated perameter
     var state = lists[newList[0]];
     splicedstuff.push(state);
   }
-
+  
 return splicedstuff;
 }
 
@@ -69,6 +69,9 @@ function makeAnd(first, second) {
 function makeOr(first, second) {
     var statementNew = {statement:first.statement + " or " + second.statement,bool:false};
     if (first.bool || second.bool) {
+      console.log(first)
+      
+      console.log(second)
         statementNew.bool = true;
         return statementNew;
     }
@@ -82,8 +85,12 @@ function makeNot(object) {
     return statementNew;
 }
 function makeComplex(list, maxLen = 5) {
+            var doNotit = 0;
   for (var k = 0; k < list.length;k++) {
-        var randMath = Math.floor(Math.random()*4)+1;
+        var randMath = Math.floor(Math.random()*5);
+        if (randMath == 0) {
+            randMath+=1;
+        }
       var itter = 0;
         while (itter < randMath) {
           var oneFourthChance = [1,2,3,4];
@@ -97,9 +104,11 @@ function makeComplex(list, maxLen = 5) {
                   }else{
                    doNot = false;
                   }
-          if (doNot === true) {
+          if (doNot === true && doNotit == 0) {
             list[k] = makeNot(list[k]);
+            doNotit++;
           }
+          
           var oneOrtwo = ["one", "two"];
             var pickRand = Math.floor(Math.random()*2);
             var pickLocate = Math.floor(Math.random()*list.length);
@@ -145,11 +154,11 @@ keys.addEventListener('click', e => {
         statements = randomList(myStatments, myStatments.length);
         stateArr = makeComplex(statements);
 
-      }
+      } 
     }
     if (action === "false") {
       if (stateArr[indexS].bool === false) {
-        score+=5;
+        score+=5; 
       }
       else{
         score-=5;
@@ -161,10 +170,9 @@ keys.addEventListener('click', e => {
         statements = randomList(myStatments, myStatments.length);
         stateArr = makeComplex(statements);
 
-      }
+      } 
 
     }
     scoreD.textContent = "Score : " + score;
   }
 })
-//hek
